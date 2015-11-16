c9fef13cc543eb3143cec11e1073b81c7a630344
ChristinaLa <christina.la.777@gmail.com>
2015-11-16 09:58:58 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "system.h"
+#include "sylvester.h"
+#include "vsylvester.h"
+#include "companion.h"
+#include <Eigen/Dense>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+	sys s(argv);
+	cout << endl << "Equations" << endl << "__________________" << endl;
+	
+//	s.print();
+
+	sylvester syl(s);
+//	syl.print_matrix();
+//	cout << endl;
+//	syl.print_pol(-1);
+	syl.print_pol(syl.getDepth()-1);
+
+	cout << "K ~~ " << syl.calculate_k() << endl;
+
+	cout << endl << "Roots" << endl << "_____________________" << endl;
+	companion c(syl);
+/*
+	int v[4] = {1,2,3,4};
+	vsylvester vsyl(syl,v);
+	vsyl.print_matrix();
+*/
+}
